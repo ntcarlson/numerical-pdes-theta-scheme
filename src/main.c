2a7c570146ea572d068423e87d4109c7fa1bd7f4
Nils Carlson <ncarlson@nmt.edu>
2019-09-16 01:04:05 -0600

    rudimentary openmp pragmas

--- src/main.c
+++ src/main.c
@@ -1,5 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <omp.h>
+#include <time.h>
 
 struct tridiag_row_t {
     double a;
@@ -80,39 +82,83 @@ void phase1(struct tridiag_t system, struct tridiag_t interface_sys, int n_part)
 
 void parallel_thomas(struct tridiag_t system, double *x, int n_part) {
     int part_size = system.n/n_part;
+    struct tridiag_row_t *A = system.A;
+
     struct tridiag_t interface_sys;
     interface_sys.A = malloc(sizeof(struct tridiag_row_t)*n_part*2);
     interface_sys.n = 2*n_part;
 
-    phase1(system, interface_sys, n_part);
+    #pragma omp parallel
+    {
+        // Compute the interface equations on each processor
+        #pragma omp for
+        for (int partition = 0; partition < n_part; partition++) {
+            int start = partition*part_size;
 
-    inplace_thomas(interface_sys, x);
+            struct tridiag_row_t *upper = &interface_sys.A[partition*2];
+            struct tridiag_row_t *lower = &interface_sys.A[partition*2+1];
 
-    for (int partition = 0; partition < n_part; partition++) {
-        system.A[partition*part_size].a = 0;
-        system.A[partition*part_size].b = 1;
-        system.A[partition*part_size].c = 0;
-        system.A[partition*part_size].d = x[2*partition];
+            upper->a = A[start + part_size - 2].a;
+            upper->b = A[start + part_size - 2].b;
+            upper->c = A[start + part_size - 2].c;
+            upper->d = A[start + part_size - 2].d;
+            for (int i = start + part_size - 3; i >= start; i--) {
+                double beta = A[i].c/upper->b;
+                upper->d = A[i].d - beta*upper->d;
+                upper->c = - beta*upper->c;
+                upper->b = A[i].b - beta*upper->a;
+                upper->a = A[i].a;
+            }
 
-        system.A[(partition + 1)*part_size - 1].a = 0;
-        system.A[(partition + 1)*part_size - 1].b = 1;
-        system.A[(partition + 1)*part_size - 1].c = 0;
-        system.A[(partition + 1)*part_size - 1].d = x[2*partition + 1];
+            lower->a = A[start + 1].a;
+            lower->b = A[start + 1].b;
+            lower->c = A[start + 1].c;
+            lower->d = A[start + 1].d;
+            for (int i = start + 2; i < start + part_size; i++) {
+                double alpha = A[i].a/lower->b;
+                lower->a = -alpha*lower->a;
+                lower->b = A[i].b - alpha*lower->c;
+                lower->c = A[i].c;
+                lower->d = A[i].d - alpha*lower->d;
+            }
+        }
+
+        // Collect and solve the interface equation on the master thread
+        #pragma omp barrier
+        if (omp_get_thread_num() == 0) {
+         inplace_thomas(interface_sys, x);
+        }
+        #pragma omp barrier
+
+        // Scatter the solved interface system among the processors
+        #pragma omp for
+        for (int partition = 0; partition < n_part; partition++) {
+            system.A[partition*part_size].a = 0;
+            system.A[partition*part_size].b = 1;
+            system.A[partition*part_size].c = 0;
+            system.A[partition*part_size].d = x[2*partition];
+
+            system.A[(partition + 1)*part_size - 1].a = 0;
+            system.A[(partition + 1)*part_size - 1].b = 1;
+            system.A[(partition + 1)*part_size - 1].c = 0;
+            system.A[(partition + 1)*part_size - 1].d = x[2*partition + 1];
+        }
+
+        // Solve the independent tridiagonal systems
+        #pragma omp for
+        for (int partition = 0; partition < n_part; partition++) {
+            struct tridiag_t sub_sys;
+            sub_sys.n = part_size;
+            sub_sys.A = &system.A[partition*part_size];
+            double *sub_x = &x[partition*part_size];
+            inplace_thomas(sub_sys, sub_x);
+        }
     }
-
-    for (int partition = 0; partition < n_part; partition++) {
-        struct tridiag_t sub_sys;
-        sub_sys.n = part_size;
-        sub_sys.A = &system.A[partition*part_size];
-        double *sub_x = &x[partition*part_size];
-        inplace_thomas(sub_sys, sub_x);
-    }
-    
 }
 
 int main() {
-    //int n = 1e8;
     int n = 1<<27;
+    //int n = 16;
     double a = 0.2;
     double *x = malloc(sizeof(double)*n);
 
@@ -130,12 +176,15 @@ int main() {
     system.A[n-1].c = 0;
 
 
+    printf("Running parallel solver\n");
+    double start = omp_get_wtime();
     parallel_thomas(system, x, 8);
+    double total = omp_get_wtime() - start;
     //inplace_thomas(system, x);
 
-    printf("%0.4f\n", x[n-1]);
+    printf("%0.4f: Total time = %f\n", x[n-1], total);
 
-    //for (int i = 0; i < n; i++) {
-    //    printf("%0.3f\n", x[i]);
-    //}
+    // for (int i = 0; i < n; i++) {
+    //     printf("%0.5f\n", x[i]);
+    // }
 }
